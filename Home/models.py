af50631a5708461f16572b42fd6232860d841de0
cristobal lopez <cristobal.lopez@upacifico.edu.py>
2023-06-02 08:48:43 -0400

    comit_001

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,17 @@
+
 from django.db import models
 
-# Create your models here.
+class cliente (models.Model):
+    nombre = models.CharField(max_length=250, null=True, blank=True)
+    apellido = models.CharField(max_length=250, null=True, blank=True)
+    direccion = models.CharField(max_length=250, null=True, blank=True)
+    telefono = models.CharField(max_length=250, null=True, blank=True)
+    estado = models.BooleanField(default=True)
+    
+class caso (models.Model):
+    cliente = models.ForeignKey(cliente, on_delete=models.CASCADE)
+    codigo_caso = models.CharField(max_length=250, null=True, blank=True)
+    observacion = models.CharField(max_length=250, null=True, blank=True)
+    departamento_actual = models.CharField(max_length=250, null=True, blank=True)
+    usuario = models.CharField(max_length=250, null=True, blank=True)
+    estado = models.BooleanField(default=True)
